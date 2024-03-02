4d72d357f9bbb2effe758b843bcffea3a6a83032
= <daniel20025febrero29@gmail.com>
2024-03-02 16:08:48 -0500

    Cors add

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 import routes from './src/routes/router.js'
 import express from 'express'
 import dotenv from './config.js'
+import cors from 'cors'
 
 dotenv.config()
 
+const optionCors = {
+    origin: '*',
+    optionsSuccessStatus: 200,
+}
+
 const app = express()
+
+app.use(cors(optionCors))
 
 app.use(express.json())
 
